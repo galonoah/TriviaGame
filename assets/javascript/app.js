a94660aa1f0a94620560b0c5cfc0123430c5fc91
Elmer C <elmer.galo@outlook.com>
2019-10-08 14:32:52 -0400

    Create variable array with object data containing questions and answers for trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,73 @@
+// Trivia questions and answers data
+var QUESTIONS = [
+    {
+        question: "What unit of measurement is abbreviated 'Oz'?",
+        answers: ["Ounces", "Pounds", "Liters", "Oblongs"],
+        correctAnswer: "Ounces",
+        image: "./assets/images/ounces.jpg"
+    },
+    {
+        question: "Which continent is the least populated?",
+        answers: ["Asia", "Antarctica", "Europe", "Africa"],
+        correctAnswer: "Antarctica",
+        image: "./assets/images/antarctica.jpg"
+    },
+    {
+        question: "In the initials of the federal agency known as NASA, what does the first 'A' stand for?",
+        answers: ["American", "Aeronautics", "Association", "Administration"],
+        correctAnswer: "Aeronautics",
+        image: "./assets/images/nasaLogo.jpg"
+    },
+    {
+        question: "What is the capital of Colorado?",
+        answers: ["Boise", "Topeka", "Denver", "Trenton"],
+        correctAnswer: "Denver",
+        image: "./assets/images/denverColorado.jpg"
+    },
+    {
+        question: "Which is the largest state in the US?",
+        answers: ["Alaska", "Texas", "Utah", "California"],
+        correctAnswer: "Alaska",
+        image: "./assets/images/alaskaPlate.jpg"
+    },
+    {
+        question: "Berlin is the capital of which country?",
+        answers: ["Belgium", "Netherlands", "Poland", "Germany"],
+        correctAnswer: "Germany",
+        image: "./assets/images/berlinCard.jpg"
+    },
+    {
+        question: "Which is the longest river in the world?",
+        answers: ["Nile", "Amazon", "Mississippi", "Yangtze"],
+        correctAnswer: "Nile",
+        image: "./assets/images/nileRiver.jpg"
+    },
+    {
+        question: "Where are the famous Egyptian Pyramids located?",
+        answers: ["Giza", "Cairo", "Thebes", "Alexandria"],
+        correctAnswer: "Giza",
+        image: "./assets/images/giza.jpg"
+    },
+    {
+        question: "Which kind of instrument is a violin?",
+        answers: ["Woodwind", "Percussion", "String", "Brass"],
+        correctAnswer: "String",
+        image: "./assets/images/violin.jpg"
+    },
+    {
+        question: "The bright stuff spewing out of a volcano is called...?",
+        answers: ["Fire", "Scoria", "Ashes", "Lava"],
+        correctAnswer: "Lava",
+        image: "./assets/images/lava.jpg"
+    },
+    {
+        question: "What fruit is traditionally the primary ingredient in wine?",
+        answers: ["Agave", "Grapes", "Peach", "Plum"],
+        correctAnswer: "Grapes",
+        image: "./assets/images/wine.jpg"
+    }
+];
+
 $(function () {
-    
+
 });
