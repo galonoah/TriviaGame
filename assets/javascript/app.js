49bb5890a4ffe819fd37b782c968307435874412
Elmer C. Galo <elmer.galo@outlook.com>
2021-07-09 01:58:57 -0400

    chore: add eslint and prettier format/styly rules

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,166 +1,170 @@
+// TODO: Refactor code
+// Check for order of functions calls to follow Eslint recommendation (no-use-before-define)
+
 // Trivia questions and answers data
-var QUESTIONS = [
+const QUESTIONS = [
   {
     question: "What unit of measurement is abbreviated 'OZ'?",
-    answers: ["Ounces", "Pounds", "Liters", "Oblongs"],
-    correctAnswer: "Ounces",
-    image: "./assets/images/ounces.jpg",
+    answers: ['Ounces', 'Pounds', 'Liters', 'Oblongs'],
+    correctAnswer: 'Ounces',
+    image: './assets/images/ounces.jpg',
   },
   {
-    question: "Which continent is the least populated?",
-    answers: ["Asia", "Antarctica", "Europe", "Africa"],
-    correctAnswer: "Antarctica",
-    image: "./assets/images/antarctica.jpg",
+    question: 'Which continent is the least populated?',
+    answers: ['Asia', 'Antarctica', 'Europe', 'Africa'],
+    correctAnswer: 'Antarctica',
+    image: './assets/images/antarctica.jpg',
   },
   {
     question:
       "In the initials of the federal agency known as NASA, what does the first 'A' stand for?",
-    answers: ["American", "Aeronautics", "Association", "Administration"],
-    correctAnswer: "Aeronautics",
-    image: "./assets/images/nasaLogo.jpg",
+    answers: ['American', 'Aeronautics', 'Association', 'Administration'],
+    correctAnswer: 'Aeronautics',
+    image: './assets/images/nasaLogo.jpg',
   },
   {
-    question: "What is the capital of Colorado?",
-    answers: ["Boise", "Topeka", "Denver", "Trenton"],
-    correctAnswer: "Denver",
-    image: "./assets/images/denverColorado.jpg",
+    question: 'What is the capital of Colorado?',
+    answers: ['Boise', 'Topeka', 'Denver', 'Trenton'],
+    correctAnswer: 'Denver',
+    image: './assets/images/denverColorado.jpg',
   },
   {
-    question: "Which is the largest state in the US?",
-    answers: ["Alaska", "Texas", "Utah", "California"],
-    correctAnswer: "Alaska",
-    image: "./assets/images/alaskaPlate.jpg",
+    question: 'Which is the largest state in the US?',
+    answers: ['Alaska', 'Texas', 'Utah', 'California'],
+    correctAnswer: 'Alaska',
+    image: './assets/images/alaskaPlate.jpg',
   },
   {
-    question: "Berlin is the capital of which country?",
-    answers: ["Belgium", "Netherlands", "Poland", "Germany"],
-    correctAnswer: "Germany",
-    image: "./assets/images/berlinCard.jpg",
+    question: 'Berlin is the capital of which country?',
+    answers: ['Belgium', 'Netherlands', 'Poland', 'Germany'],
+    correctAnswer: 'Germany',
+    image: './assets/images/berlinCard.jpg',
   },
   {
-    question: "Which is the longest river in the world?",
-    answers: ["Nile", "Amazon", "Mississippi", "Yangtze"],
-    correctAnswer: "Nile",
-    image: "./assets/images/nileRiver.jpg",
+    question: 'Which is the longest river in the world?',
+    answers: ['Nile', 'Amazon', 'Mississippi', 'Yangtze'],
+    correctAnswer: 'Nile',
+    image: './assets/images/nileRiver.jpg',
   },
   {
-    question: "Where are the famous Egyptian Pyramids located?",
-    answers: ["Giza", "Cairo", "Thebes", "Alexandria"],
-    correctAnswer: "Giza",
-    image: "./assets/images/giza.jpg",
+    question: 'Where are the famous Egyptian Pyramids located?',
+    answers: ['Giza', 'Cairo', 'Thebes', 'Alexandria'],
+    correctAnswer: 'Giza',
+    image: './assets/images/giza.jpg',
   },
   {
-    question: "Which kind of instrument is a violin?",
-    answers: ["Woodwind", "Percussion", "String", "Brass"],
-    correctAnswer: "String",
-    image: "./assets/images/violin.jpg",
+    question: 'Which kind of instrument is a violin?',
+    answers: ['Woodwind', 'Percussion', 'String', 'Brass'],
+    correctAnswer: 'String',
+    image: './assets/images/violin.jpg',
   },
   {
-    question: "The bright stuff spewing out of a volcano is called...?",
-    answers: ["Fire", "Scoria", "Ashes", "Lava"],
-    correctAnswer: "Lava",
-    image: "./assets/images/lava.jpg",
+    question: 'The bright stuff spewing out of a volcano is called...?',
+    answers: ['Fire', 'Scoria', 'Ashes', 'Lava'],
+    correctAnswer: 'Lava',
+    image: './assets/images/lava.jpg',
   },
   {
-    question: "What fruit is traditionally the primary ingredient in wine?",
-    answers: ["Agave", "Grapes", "Peach", "Plum"],
-    correctAnswer: "Grapes",
-    image: "./assets/images/wine.jpg",
+    question: 'What fruit is traditionally the primary ingredient in wine?',
+    answers: ['Agave', 'Grapes', 'Peach', 'Plum'],
+    correctAnswer: 'Grapes',
+    image: './assets/images/wine.jpg',
   },
-];
+]
 
-var shuffleQuestions;
-var correctAnswerElement;
-var questionIndex = 0;
-var timeLeft = 15;
-var timer;
-var correct = 0;
-var incorrect = 0;
-var isAnswerImageShowing = false;
+let shuffleQuestions
+let correctAnswerElement
+let questionIndex = 0
+let timeLeft = 15
+let timer
+let correct = 0
+let incorrect = 0
+let isAnswerImageShowing = false
 
 // Shuffle list array
 const shuffle = ([...arr]) => {
-  let m = arr.length;
+  let m = arr.length
+  const shuffleArr = arr
   while (m) {
-    const i = Math.floor(Math.random() * m--);
-    [arr[m], arr[i]] = [arr[i], arr[m]];
+    const i = Math.floor(Math.random() * m--)
+    ;[shuffleArr[m], shuffleArr[i]] = [arr[i], arr[m]]
   }
-  return arr;
-};
+  return shuffleArr
+}
 
 // show image expanding effect only on mobile view
 function showImageOnMobile() {
-  $("#card-trivia__answer-image").addClass("expand-image");
-  $(".flip-box-inner").addClass("flip-answer-image");
+  $('#card-trivia__answer-image').addClass('expand-image')
+  $('.flip-box-inner').addClass('flip-answer-image')
   setTimeout(() => {
-    $("#card-trivia__answer-image").removeClass("expand-image");
-    $(".flip-box-inner").removeClass("flip-answer-image");
-  }, 3800);
+    $('#card-trivia__answer-image').removeClass('expand-image')
+    $('.flip-box-inner').removeClass('flip-answer-image')
+  }, 3800)
 }
 
 // handle answer-image visibility on windows resizing
-$(window).on("resize", function () {
-  var win = $(this); //this = window
+$(window).on('resize', () => {
+  const win = $(this) // this = window
   if (win.width() <= 650 && isAnswerImageShowing) {
-    $(".flip-box-inner").addClass("flip-answer-image");
-    $("#card-trivia__answer-image").addClass("expand-image");
+    $('.flip-box-inner').addClass('flip-answer-image')
+    $('#card-trivia__answer-image').addClass('expand-image')
   }
 
   if (win.width() > 650 && isAnswerImageShowing) {
-    $(".flip-box-inner").addClass("flip-answer-image");
-    $("#card-trivia__answer-image").removeClass("expand-image");
+    $('.flip-box-inner').addClass('flip-answer-image')
+    $('#card-trivia__answer-image').removeClass('expand-image')
   }
-});
+})
 
-$(function () {
+$(() => {
   // Hide main HTML content until background image loads
-  $("#container").hide();
-  var src = $("#container").css("background-image");
-  var url = src.match(/\((.*?)\)/)[1].replace(/('|")/g, "");
-  var img = new Image();
-  img.onload = function () {
+  $('#container').hide()
+  const src = $('#container').css('background-image')
+  const url = src.match(/\((.*?)\)/)[1].replace(/('|")/g, '')
+  const img = new Image()
+  img.onload = () => {
     //  load app background image
-    $(".looping-rhombuses-spinner").hide();
-    $("#container").show();
-  };
-  img.src = url;
-  if (img.complete) img.onload();
+    $('.looping-rhombuses-spinner').hide()
+    $('#container').show()
+  }
+  img.src = url
+  if (img.complete) img.onload()
 
   // Function gets Questions-Data and append it to HTML and
   // returns element containing correct answer
   function getTrivia() {
-    let correctAnswer;
+    let correctAnswer
 
     // delay appending answer-image after flip effect
     setTimeout(() => {
-      $(".flip-box-back")
+      $('.flip-box-back')
         .empty()
         .append(
           `<img src='${shuffleQuestions[questionIndex].image}' alt='answer'>`
-        );
-    }, 500);
+        )
+    }, 500)
 
     // append trivia question
-    $("#card-trivia__question").text(shuffleQuestions[questionIndex].question);
+    $('#card-trivia__question').text(shuffleQuestions[questionIndex].question)
 
     // get trivia possible answers
-    const shuffleAnswers = shuffle(shuffleQuestions[questionIndex].answers);
+    const shuffleAnswers = shuffle(shuffleQuestions[questionIndex].answers)
 
     // remove previews answers and append new answers for next trivia
-    $("#card-trivia__answers").empty();
-    shuffleAnswers.forEach(function (answer) {
-      const answerElement = $("<div>").addClass(
-        "text-center card-trivia__answer--hover-effect"
-      );
-      answerElement.append(answer);
-      answerElement.appendTo($("#card-trivia__answers"));
+    $('#card-trivia__answers').empty()
+    shuffleAnswers.forEach((answer) => {
+      const answerElement = $('<div>').addClass(
+        'text-center card-trivia__answer--hover-effect'
+      )
+      answerElement.append(answer)
+      answerElement.appendTo($('#card-trivia__answers'))
 
       if (answer === shuffleQuestions[questionIndex].correctAnswer) {
-        correctAnswer = answerElement;
+        correctAnswer = answerElement
       }
-    });
+    })
 
-    return correctAnswer;
+    return correctAnswer
   }
 
   // Function adds click event to each answer element and
@@ -168,118 +172,118 @@ $(function () {
   // correct answer, text turns green and appends check mark
   // else text turns color gray and appends an X mark
   function checkCorrectAnswer() {
-    $("#card-trivia__answers")
+    $('#card-trivia__answers')
       .children()
-      .click(function () {
-        isAnswerImageShowing = true;
-        resetIntervalTimer();
+      .click(function checkAnswersFn() {
+        isAnswerImageShowing = true
+        resetIntervalTimer()
 
         if ($(this).text() === correctAnswerElement.text()) {
-          correct++;
-          showCorrectAnswer();
+          correct++
+          showCorrectAnswer()
         } else {
-          incorrect++;
-          $(this).addClass("wrong");
-          $(this).append("<span> &#10008;</span>");
-          $(this).siblings().not(correctAnswerElement).addClass("wrong");
+          incorrect++
+          $(this).addClass('wrong')
+          $(this).append('<span> &#10008;</span>')
+          $(this).siblings().not(correctAnswerElement).addClass('wrong')
 
-          correctAnswerElement.append("<span> &#10004;</span>");
-          correctAnswerElement.addClass("correct");
+          correctAnswerElement.append('<span> &#10004;</span>')
+          correctAnswerElement.addClass('correct')
 
-          $("#message").css("display", "block");
-          $("#timeLeft").css("display", "none");
-          $("#message").text("INCORRECT");
+          $('#message').css('display', 'block')
+          $('#timeLeft').css('display', 'none')
+          $('#message').text('INCORRECT')
         }
 
         // show image-answer with flip effect
         if ($(window).width() > 650) {
-          $(".flip-box-inner, .flip-box-back").addClass("flip-answer-image");
+          $('.flip-box-inner, .flip-box-back').addClass('flip-answer-image')
         }
 
         // Prevent click event and hover effect after answer selection
         $(this)
           .parent()
           .children()
-          .off("click")
-          .removeClass("card-trivia__answer--hover-effect");
+          .off('click')
+          .removeClass('card-trivia__answer--hover-effect')
 
         if ($(window).width() <= 650) {
-          showImageOnMobile();
+          showImageOnMobile()
         }
-      });
+      })
   }
 
   // Function shows the correct trivia answer
   function showCorrectAnswer() {
-    $("#card-trivia__answers")
+    $('#card-trivia__answers')
       .children()
       .not(correctAnswerElement)
-      .addClass("wrong");
-    correctAnswerElement.append("<span> &#10004;</span>");
-    correctAnswerElement.addClass("correct");
+      .addClass('wrong')
+    correctAnswerElement.append('<span> &#10004;</span>')
+    correctAnswerElement.addClass('correct')
 
-    $("#card-trivia__answers").children().removeClass("hover");
-    $("#card-trivia__answers").children().off("click");
+    $('#card-trivia__answers').children().removeClass('hover')
+    $('#card-trivia__answers').children().off('click')
 
-    $("#message").css("display", "block");
-    $("#timeLeft").css("display", "none");
-    $("#message").text("CORRECT");
-    $(".flip-box-inner, .flip-box-back").addClass("flip-answer-image");
-    $("#card-trivia__answer-image").removeClass("expand-image");
+    $('#message').css('display', 'block')
+    $('#timeLeft').css('display', 'none')
+    $('#message').text('CORRECT')
+    $('.flip-box-inner, .flip-box-back').addClass('flip-answer-image')
+    $('#card-trivia__answer-image').removeClass('expand-image')
   }
 
   // Function creates a countdown which is display on HTML
   // If timeLeft variable reach zero, correct answer shows up
   // and then wait for 4.5 seconds to restart
   function intervalTimer() {
-    timer = setInterval(function () {
-      timeLeft--;
-      $("#timerSeconds").text(timeLeft);
+    timer = setInterval(() => {
+      timeLeft--
+      $('#timerSeconds').text(timeLeft)
 
-      if (timeLeft == 0) {
-        incorrect++;
-        showCorrectAnswer();
-        clearInterval(timer);
-        timeLeft = 15;
-        $("#timeLeft").css("display", "none");
-        $("#message").text("TIME'S UP!");
-        $("#message").css("display", "block");
+      if (timeLeft === 0) {
+        incorrect++
+        showCorrectAnswer()
+        clearInterval(timer)
+        timeLeft = 15
+        $('#timeLeft').css('display', 'none')
+        $('#message').text("TIME'S UP!")
+        $('#message').css('display', 'block')
 
         if ($(window).width() <= 650) {
-          showImageOnMobile();
+          showImageOnMobile()
         }
 
-        setTimeout(function () {
-          questionIndex++;
-          $("#message").css("display", "none");
-          $("#timeLeft").css("display", "block");
-          $("#timerSeconds").text(timeLeft);
-          $(".flip-box-inner, .flip-box-back").removeClass("flip-answer-image");
-          $("#card-trivia__answer-image").removeClass("expand-image");
-          isAnswerImageShowing = false;
-          startGame();
-        }, 4500);
+        setTimeout(() => {
+          questionIndex++
+          $('#message').css('display', 'none')
+          $('#timeLeft').css('display', 'block')
+          $('#timerSeconds').text(timeLeft)
+          $('.flip-box-inner, .flip-box-back').removeClass('flip-answer-image')
+          $('#card-trivia__answer-image').removeClass('expand-image')
+          isAnswerImageShowing = false
+          startGame()
+        }, 4500)
       }
-    }, 1000);
+    }, 1000)
   }
 
   // Function clears countdown after an answer is click and
   // waits four seconds to restart
   function resetIntervalTimer() {
-    clearInterval(timer);
+    clearInterval(timer)
 
-    setTimeout(function () {
-      questionIndex++;
-      timeLeft = 15;
-      $("#message").css("display", "none");
-      $("#message").text("TIME'S UP");
-      $("#timeLeft").css("display", "block");
-      $("#timerSeconds").text(timeLeft);
-      $(".flip-box-inner, .flip-box-back").removeClass("flip-answer-image");
-      $("#card-trivia__answer-image").removeClass("expand-image");
-      isAnswerImageShowing = false;
-      startGame();
-    }, 4500);
+    setTimeout(() => {
+      questionIndex++
+      timeLeft = 15
+      $('#message').css('display', 'none')
+      $('#message').text("TIME'S UP")
+      $('#timeLeft').css('display', 'block')
+      $('#timerSeconds').text(timeLeft)
+      $('.flip-box-inner, .flip-box-back').removeClass('flip-answer-image')
+      $('#card-trivia__answer-image').removeClass('expand-image')
+      isAnswerImageShowing = false
+      startGame()
+    }, 4500)
   }
 
   // Function starts the game
@@ -287,35 +291,35 @@ $(function () {
   // shows up, otherwise the games keeps running until last question
   function startGame() {
     if (questionIndex < shuffleQuestions.length) {
-      correctAnswerElement = getTrivia();
+      correctAnswerElement = getTrivia()
 
-      checkCorrectAnswer();
+      checkCorrectAnswer()
 
-      intervalTimer();
+      intervalTimer()
     } else {
-      questionIndex = 0;
-      isAnswerImageShowing = false;
-      $("#card-trivia").css("display", "none");
-      $("#correct").text("Correct: " + correct);
-      $("#incorrect").text("Incorrect: " + incorrect);
-      $("#score").css("display", "block");
+      questionIndex = 0
+      isAnswerImageShowing = false
+      $('#card-trivia').css('display', 'none')
+      $('#correct').text(`Correct: ${correct}`)
+      $('#incorrect').text(`Incorrect: ${incorrect}`)
+      $('#score').css('display', 'block')
     }
   }
 
   // Click event button starts game
-  $("#button").click(function () {
-    correct = 0;
-    incorrect = 0;
-    questionIndex = 0;
-    $("#card-trivia").css("display", "grid");
-    $("#button").css("display", "none");
-    shuffleQuestions = shuffle(QUESTIONS);
-    startGame();
-  });
+  $('#button').click(() => {
+    correct = 0
+    incorrect = 0
+    questionIndex = 0
+    $('#card-trivia').css('display', 'grid')
+    $('#button').css('display', 'none')
+    shuffleQuestions = shuffle(QUESTIONS)
+    startGame()
+  })
 
   // Click event close score pop-up
-  $("#close").click(function () {
-    $("#score").css("display", "none");
-    $("#button").css("display", "inline-block");
-  });
-});
+  $('#close').click(() => {
+    $('#score').css('display', 'none')
+    $('#button').css('display', 'inline-block')
+  })
+})
